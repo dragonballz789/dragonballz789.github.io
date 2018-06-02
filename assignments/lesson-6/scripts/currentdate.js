52360b0198fb4277f105dd04771f82d8cd6982a9
dragonballz789 <38597123+dragonballz789@users.noreply.github.com>
2018-06-02 12:07:44 -0600

    d

--- assignments/lesson-6/scripts/currentdate.js
+++ assignments/lesson-6/scripts/currentdate.js
@@ -1,12 +1,17 @@
+'use strict';
 
-    function currentdate() {
-        var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        var number = getdate();
-        var year = getFullYear();
-        var month = getMonth();
-        var days = day[number.getDay()];
+function currentdate() {
+    var d = new Date();
     
+    var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var days = day[d.getDay()];
+    var year = d.getFullYear();
+   
+    
+    var months = ["January", "February", "March", "April", "May", "June", "July", "Auguest", "September", "October", "November", "December"];
+    var maybe = months[d.getMonth()];
+    var please = days + "," + " " + d.getDate() + " " + maybe + " " + year;
 
-    document.getElementById('onscreen').innerHTML = days;
+    
+    document.getElementById('currentdate()').innerHTML = please;
 }
-
